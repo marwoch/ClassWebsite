c581353c7b3d4cae55024c4545867da207f81d52
marwoch <marwoch@users.noreply.github.com>
2017-07-12 13:24:10 -0700

    More work on the registration form

--- ClassWebsite/Models/Member.cs
+++ ClassWebsite/Models/Member.cs
@@ -7,5 +7,28 @@ namespace ClassWebsite.Models
 {
     public class Member
     {
+        //email
+        //first name
+        //last name
+        //SecurityQuestion
+        public int MemberID { get; set; } //primary key
+
+        public string UserName { get; set; } //req
+
+        public string Password { get; set; } //req
+
+        public DateTime? DateOfBirth { get; set; } //nullable
+
+        public string FirstName { get; set; } //req
+
+        public string LastName { get; set; } //null
+
+        public string SecurityQuestion { get; set; } //req
+
+        public string Email { get; set; } //req
+
+
+
+
     }
 }
